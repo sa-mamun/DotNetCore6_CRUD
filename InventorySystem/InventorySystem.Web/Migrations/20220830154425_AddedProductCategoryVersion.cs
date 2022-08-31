776dcce4d53fbacdf6452421974cb5df66c72b54
sa-mamun <samamun009@gmail.com>
2022-08-31 22:47:43 +0600

    Migrations added

---
+++ InventorySystem/InventorySystem.Web/Migrations/20220830154425_AddedProductCategoryVersion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventorySystem.Web.Migrations
+{
+    public partial class AddedProductCategoryVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "VersionNumber",
+                table: "Products",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "VersionNumber",
+                table: "Categories",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VersionNumber",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "VersionNumber",
+                table: "Categories");
+        }
+    }
+}
