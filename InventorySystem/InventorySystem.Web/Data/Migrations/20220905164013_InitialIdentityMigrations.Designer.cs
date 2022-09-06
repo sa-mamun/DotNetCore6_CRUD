8c7f78f4996bf6b7e3e46401537bc38412377835
sa-mamun <samamun009@gmail.com>
2022-09-06 21:26:50 +0600

    identity added

--- InventorySystem/InventorySystem.Web/Data/Migrations/20220905164013_InitialIdentityMigrations.Designer.cs
+++ InventorySystem/InventorySystem.Web/Data/Migrations/20220905164013_InitialIdentityMigrations.Designer.cs
@@ -4,6 +4,7 @@ using InventorySystem.Membership;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventorySystem.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220905164013_InitialIdentityMigrations")]
+    partial class InitialIdentityMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- InventorySystem/InventorySystem.Web/Program.cs
+++ InventorySystem/InventorySystem.Web/Program.cs
@@ -2,7 +2,9 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using InventorySystem.Core;
 using InventorySystem.Core.Contexts;
-using InventorySystem.Web.Data;
+using InventorySystem.Membership;
+using InventorySystem.Membership.Entities;
+using InventorySystem.Membership.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,8 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var migrationAssemblyName = typeof(Program).Assembly.FullName;
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    options.UseSqlServer(connectionString, b =>
+        b.MigrationsAssembly(migrationAssemblyName)));
 
 builder.Services.AddDbContext<CoreDbContext>(options =>
     options.UseSqlServer(connectionString, b =>
@@ -23,12 +25,47 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
     //containerBuilder.RegisterModule(new WebModule());
-    //containerBuilder.RegisterModule(new MembershipModule(connectionString, migrationAssemblyName));
+    containerBuilder.RegisterModule(new MembershipModule(connectionString, migrationAssemblyName));
     containerBuilder.RegisterModule(new CoreModule(connectionString, migrationAssemblyName));
 });
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add services to the container.
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services
+    .AddIdentity<ApplicationUser, Role>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddUserManager<UserManager>()
+    .AddRoleManager<RoleManager>()
+    .AddSignInManager<SignInManager>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
