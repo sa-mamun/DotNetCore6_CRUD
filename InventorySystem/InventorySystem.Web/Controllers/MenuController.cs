8799a27e100aee71e8e1a89db48ad7fd4d240289
maaaruf <marufbdonline@gmail.com>
2022-09-30 10:59:45 +0600

    Added MenuController

---
+++ InventorySystem/InventorySystem.Web/Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystem.Web.Controllers
+{
+    public class MenuController : BaseController
+    {
+        public MenuController(IAuthorizationService authorizationService)
+            :base(authorizationService)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
